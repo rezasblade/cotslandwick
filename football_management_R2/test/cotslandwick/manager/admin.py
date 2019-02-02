5c9cbf456474c6acf94437ee69061b8294be2d11
Reza <Rezasblade@gmail.com>
2019-02-02 13:17:37 +0000

    functional

---
+++ football_management_R2/test/cotslandwick/manager/admin.py
@@ -0,0 +1,31 @@
+from django.contrib import admin
+
+from .models import Player, Team, Match
+
+admin.site.register(Player)
+admin.site.register(Team)
+admin.site.register(Match)
+
+
+# from .models import Player
+# from .models import MatchDay
+# from .models import Results
+# from .models import PlayerPoints
+
+# # Register your models here.
+
+
+# admin.site.register(Player)
+# admin.site.register(MatchDay)
+# admin.site.register(Results)
+# # admin.site.register(PlayerPoints)
+
+# # Define the admin class
+
+
+# class PlayerPointsAdmin(admin.ModelAdmin):
+#     pass
+
+
+# # Register the admin class with the associated model
+# admin.site.register(PlayerPoints, PlayerPointsAdmin)
