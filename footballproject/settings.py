eb48a81cd902d0045251825827b512fa8eb100e6
rezasblade <Rezasblade@gmail.com>
2019-02-11 14:25:54 +0000

    Added staticroot and allowedhost of colneycots.pythonanywhere.com

--- footballproject/settings.py
+++ footballproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = ')4hb7nd%u$*^5b#wsag@+*mp!%f_f47$teb%6886ac!=r6$m=)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['colneycots.pythonanywhere.com']
 
 
 # Application definition
@@ -121,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
